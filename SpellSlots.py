bf57a64b5e01d73ea50bd114a7fa3d8e777555f5
sawells3 <sawells3@ncsu.edu>
2018-10-01 15:23:54 -0400

    Style cleanup

--- SpellSlots.py
+++ SpellSlots.py
@@ -4,47 +4,49 @@
 #4th	6
 #5th	7
 
-def AddToAllSlots(allSlots, curSlots, slotToAdd):
-	for i in range(len(curSlots)):
-		if(slotToAdd >=0 and slotToAdd <=4):
-			curSlots[i][slotToAdd]+=1
-		allSlots.append(curSlots[i])
-	return allSlots
-	
-def OneSlot(spLimit, spLeft, rechargesLeft):
-	if(spLimit >= spLeft+2 and rechargesLeft != 0):
-		return OneSlot(spLimit, spLeft+2, rechargesLeft-1)
-	allSlots = []
-		
-	if(rechargesLeft > 0 and spLimit > spLeft):
-		#At this point we know spLeft is spLimit-1
-		curSlots = OneSlot(spLimit, spLeft+1, rechargesLeft-1)
-		allSlots = AddToAllSlots(allSlots, curSlots, -1)
-	
-	if(spLeft >= 7):
-		curSlots = OneSlot(spLimit, spLeft-7, rechargesLeft)
-		allSlots = AddToAllSlots(allSlots, curSlots, 4)
-	
-	if(spLeft >= 6):
-		curSlots = OneSlot(spLimit, spLeft-6, rechargesLeft)
-		allSlots = AddToAllSlots(allSlots, curSlots, 3)
-		
-	if(spLeft >= 5):
-		curSlots = OneSlot(spLimit, spLeft-5, rechargesLeft)
-		allSlots = AddToAllSlots(allSlots, curSlots, 2)
-		
-	if(spLeft >= 3):
-		curSlots = OneSlot(spLimit, spLeft-3, rechargesLeft)
-		allSlots = AddToAllSlots(allSlots, curSlots, 1)
-		
-	if(spLeft >= 2):
-		curSlots = OneSlot(spLimit, spLeft-2, rechargesLeft)
-		allSlots = AddToAllSlots(allSlots, curSlots, 0)
-	
-	if(rechargesLeft == 0 and spLeft < 2):
+
+def add_to_all_slots(all_slots: list, cur_slots: list, slot_to_add: int) -> list:
+	for i in range(len(cur_slots)):
+		if 0 <= slot_to_add <= 4:
+			cur_slots[i][slot_to_add] += 1
+		all_slots.append(cur_slots[i])
+	return all_slots
+
+
+def one_slot(sp_limit, sp_remaining, recharges_left) -> list:
+	if sp_limit >= sp_remaining+2 and recharges_left != 0:
+		return one_slot(sp_limit, sp_remaining + 2, recharges_left - 1)
+	all_slots = []
+
+	if recharges_left > 0 and sp_limit > sp_remaining:
+		# At this point we know sp_remaining is sp_limit-1
+		cur_slots = one_slot(sp_limit, sp_remaining + 1, recharges_left - 1)
+		all_slots = add_to_all_slots(all_slots, cur_slots, -1)
+
+	if sp_remaining >= 7:
+		cur_slots = one_slot(sp_limit, sp_remaining - 7, recharges_left)
+		all_slots = add_to_all_slots(all_slots, cur_slots, 4)
+
+	if sp_remaining >= 6:
+		cur_slots = one_slot(sp_limit, sp_remaining - 6, recharges_left)
+		all_slots = add_to_all_slots(all_slots, cur_slots, 3)
+
+	if sp_remaining >= 5:
+		cur_slots = one_slot(sp_limit, sp_remaining - 5, recharges_left)
+		all_slots = add_to_all_slots(all_slots, cur_slots, 2)
+
+	if sp_remaining >= 3:
+		cur_slots = one_slot(sp_limit, sp_remaining - 3, recharges_left)
+		all_slots = add_to_all_slots(all_slots, cur_slots, 1)
+
+	if sp_remaining >= 2:
+		cur_slots = one_slot(sp_limit, sp_remaining - 2, recharges_left)
+		all_slots = add_to_all_slots(all_slots, cur_slots, 0)
+
+	if recharges_left == 0 and sp_remaining < 2:
 		return [[0,0,0,0,0]]
-	return allSlots
-	
-finalSlots = OneSlot(int(raw_input("SP Limit: ")), 0, int(raw_input("Hours of downtime: "))*2)
+	return all_slots
+
+finalSlots = one_slot(int(input("SP Limit: ")), 0, int(input("Hours of downtime: ")) * 2)
 finalSlots = [finalSlots[i] for i in range(len(finalSlots)) if i == 0 or finalSlots[i] != finalSlots[i-1]]     #removes duplicate elements
 print(finalSlots)
